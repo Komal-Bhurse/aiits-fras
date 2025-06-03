1f4320e8216f786a411402c4dd641f10a907c3a8
Pratik Kothare <kotharepratik129@gmail.com>
2025-06-03 18:06:50 +0530

    seventh commit

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,9 @@
 export const getCollegeHoursRange = () => {
   const start = new Date();
-  start.setHours(10, 0, 0, 0); // Today at 10:00:00 AM
+  start.setHours(0, 0, 0, 0); // Today at 10:00:00 AM
 
   const end = new Date();
-  end.setHours(17, 0, 0, 0); // Today at 5:00:00 PM
+  end.setHours(23, 59, 59, 999); // Today at 5:00:00 PM
 
   return { start, end };
 };
